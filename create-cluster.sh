173092f3bea592d0c2860eaf1a8a3bb396f974f4
Rick(Rugui) Chen <rickruguichen@google.com>
2023-12-02 14:19:48 -0500

    deepspeed image

--- create-cluster.sh
+++ create-cluster.sh
@@ -14,4 +14,4 @@ gcloud container clusters create llm-inference-l4 --location ${REGION} \
   --machine-type n2d-standard-4 \
   --num-nodes 1 --min-nodes 1 --max-nodes 3
 
-gcloud container node-pools create llm-inference-pool --cluster llm-inference-l4  --accelerator type=nvidia-tesla-t4,count=2,gpu-driver-version=latest   --machine-type n1-standard-4   --ephemeral-storage-local-ssd=count=2   --enable-autoscaling --enable-image-streaming   --num-nodes=0 --min-nodes=0 --max-nodes=3   --shielded-secure-boot   --shielded-integrity-monitoring   --node-locations $REGION-b --region $REGION --spot
+gcloud container node-pools create llm-inference-pool --cluster llm-inference-l4  --accelerator type=nvidia-tesla-t4,count=1,gpu-driver-version=latest   --machine-type n1-standard-4   --ephemeral-storage-local-ssd=count=1   --enable-autoscaling --enable-image-streaming   --num-nodes=0 --min-nodes=0 --max-nodes=3   --shielded-secure-boot   --shielded-integrity-monitoring   --node-locations $REGION-b --region $REGION --spot
