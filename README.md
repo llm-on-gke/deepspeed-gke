2c0f5cfa52bcb5b9c77dfdc60c37e303ac06092c
Rick(Rugui) Chen <rickruguichen@google.com>
2023-12-02 12:40:47 -0500

    deepspeed image

--- README.md
+++ README.md
@@ -9,8 +9,16 @@ Create cluster
 ```
 gcloud container clusters get-credentials llama2-inference-cluster --zone us-west1-b
 export HF_TOKEN=<paste-your-own-token>
-kubectl create secret generic llama2 --from-literal="HF_TOKEN=$HF_TOKEN" -n triton
+kubectl create secret generic huggingface --from-literal="HF_TOKEN=$HF_TOKEN" -n triton
 kubectl apply -f llama2-gke-deploy.yaml -n triton
 ```
 
 helm install deepspeed -f values.yaml oci://registry-1.docker.io/bitnamicharts/deepspeed
+
+## Common issues:
+
+CUDA initialization, GPU not found issues:
+
+import torch
+available_gpus = [torch.cuda.device(i) for i in range(torch.cuda.device_count())]
+available_gpus
--- create-cluster.sh
+++ create-cluster.sh
@@ -7,10 +7,11 @@ gcloud container clusters create llm-inference-l4 --location ${REGION} \
   --shielded-secure-boot --shielded-integrity-monitoring \
   --enable-ip-alias \
   --node-locations=$REGION-a \
+  --cluster-version=1.27.5-gke.200
   --workload-pool=${PROJECT_ID}.svc.id.goog \
   --addons GcsFuseCsiDriver   \
   --no-enable-master-authorized-networks \
   --machine-type n2d-standard-4 \
-  --num-nodes 0 --min-nodes 1 --max-nodes 3
+  --num-nodes 1 --min-nodes 1 --max-nodes 3
 
 gcloud container node-pools create llm-inference-pool --cluster llm-inference-l4  --accelerator type=nvidia-tesla-t4,count=2,gpu-driver-version=latest   --machine-type n1-standard-4   --ephemeral-storage-local-ssd=count=2   --enable-autoscaling --enable-image-streaming   --num-nodes=0 --min-nodes=0 --max-nodes=3   --shielded-secure-boot   --shielded-integrity-monitoring   --node-locations $REGION-b --region $REGION --spot
